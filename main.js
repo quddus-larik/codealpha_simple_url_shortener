c41abc4242a2f4f72cebc1f7a07ff3fd7f162c08
QUDDUS <qudduslarek@proton.me>
2026-06-03 16:43:57 +0500

    refine backend using middlewares

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 const express = require('express');
 
 const app = express();
-const PORT = process.env.PORT | 8080;
+const PORT = process.env.PORT || 8080;
 
-app.get("/",(req,res)=>{
+const urlRoute = require('./src/routes/url.route');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get("/", (req, res) => {
     res.send("Server Is On GET")
 });
+app.use('/url', urlRoute) // POST /url/create | GET /url/
 
-app.listen(PORT,()=> console.log(`server is listen on http://localhost:${PORT}`))
+app.listen(PORT, () => console.log(`server is listen on http://localhost:${PORT}`))
